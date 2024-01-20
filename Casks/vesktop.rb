1386000627b3ff28a8f75991c0727effe00da591
Michal Vaniš <zirixcz@gmail.com>
2024-01-20 11:23:18 +0100

    bump Vesktop to 1.5.0

--- Casks/vesktop.rb
+++ Casks/vesktop.rb
@@ -1,7 +1,7 @@
 cask "vesktop" do
-  version "0.4.4"
-  sha256 on_arch_conditional arm: "4665fa89ca9ab3aaad2d216697d6a513b01247bf3159e1aec73cd9bd9c873644",
-    intel: "64f9b28e4bb6509f6b930cad89be4eb66910fec653291316599b075d52b35860"
+  version "1.5.0"
+  sha256 on_arch_conditional arm: "16dba322c4f41e1d303293ef94a66654943918f61806225bfce2a2eadd978a1d",
+    intel: "b76a61b50dfe0fde6f1900383972f510af0ed77e2b018d6cd7ba5f9a08c29374"
 
   url on_arch_conditional arm: "https://github.com/Vencord/Vesktop/releases/download/v#{version}/Vesktop-#{version}-arm64.dmg",
     intel: "https://github.com/Vencord/Vesktop/releases/download/v#{version}/Vesktop-#{version}.dmg"
